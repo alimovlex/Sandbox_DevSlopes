5c2dbe9dd74274d155242ce7969306eaabfe3d22
robot <robot@recompile.me>
2025-07-07 14:56:52 +0200

    The lesson is complete!

--- Android/Sandbox/playground/src/main/java/com/example/playground/ScratchPad.java
+++ Android/Sandbox/playground/src/main/java/com/example/playground/ScratchPad.java
@@ -1,7 +1,32 @@
 package com.example.playground;
 
 public class ScratchPad {
+    public static void first_lesson() {
+        String name = "John";
+        int homeRuns = 55;
+        float bankBalance = 100.44f;
+        float finalBalance = bankBalance - 50;
+        System.out.println(name + " has " + finalBalance + " in his account!\n");
+    }
+
+    public static void second_lesson() {
+        String vehicle = "Truck";
+        String make = "Chevy";
+        String model = "Tahoe";
+
+        String fullVehicleDetails = vehicle + " - " + make + " : " + model;
+        System.out.println(fullVehicleDetails);
+
+        String upper = "CRAZY BEAST!";
+        String lower = upper.toLowerCase();
+        System.out.println(lower);
+
+        int accountBalance = 500;
+        String printBalance = String.format("Your account balance is %d", accountBalance);
+        System.out.println(printBalance);
+    }
     public static void main(String[] args) {
-        System.out.println("Hello, Nurse!");
+        ScratchPad.first_lesson();
+        ScratchPad.second_lesson();
     }
 }
